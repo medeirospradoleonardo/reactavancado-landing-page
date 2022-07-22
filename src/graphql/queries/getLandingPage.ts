d2248f81375d1734d1957b28bfe61b714d1ea654
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-07-22 03:01:12 -0300

    75. Tornando o SectionTech dinâmico

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -33,11 +33,24 @@ fragment sectionAboutProject on LandingPage{
   }
 }
 
+fragment sectionTech on LandingPage{
+  sectionTech{
+    title
+    techIcons{
+      title
+      icon{
+        url
+      }
+    }
+  }
+}
+
 query GET_LANDING_PAGE {
   landingPage {
     ...logo
     ...header
     ...sectionAboutProject
+    ...sectionTech
   }
 }
 
